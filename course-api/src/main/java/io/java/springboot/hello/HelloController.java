96528a5399beedb76f2518ad12fa3f04fa88ba52
yogesh-ss <sssyogesh@yahoo.com>
2017-05-06 23:58:29 +0100

    sample

---
+++ course-api/src/main/java/io/java/springboot/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.java.springboot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String SayHello() {
+		return "Hello, This is my first Spring Boot application";
+	}
+}
