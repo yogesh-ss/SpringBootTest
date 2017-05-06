c6b77c27be63354dd37871c04d9599a63d90a963
yogesh-ss <sssyogesh@yahoo.com>
2017-05-07 00:00:20 +0100

    sample

---
+++ eureka-client/src/main/java/com/eureka/client/demo/EurekaClientApplication.java
@@ -0,0 +1,37 @@
+package com.eureka.client.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+	        SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	 
+	    @RequestMapping("/greeting")
+	    public String greeting() {
+	        return "Hello from EurekaClient!";
+	    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public String serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getServices().get(0).toString();
+    }
+}
---
+++ eureka-server/src/main/java/com/eureka/server/demo/EurekaServerApplcation.java
@@ -0,0 +1,16 @@
+package com.eureka.server.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplcation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplcation.class, args);
+
+	}
+
+}
