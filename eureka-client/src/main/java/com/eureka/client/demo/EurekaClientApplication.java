5603b99395bfc27256faa3734d09b188f89e9b82
yogesh-ss <sssyogesh@yahoo.com>
2017-09-23 09:59:36 +0100

    added new prop file

--- eureka-client/src/main/java/com/eureka/client/demo/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/eureka/client/demo/EurekaClientApplication.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class EurekaClientApplication {
-
+	
 	public static void main(String[] args) {
 	        SpringApplication.run(EurekaClientApplication.class, args);
 	}
